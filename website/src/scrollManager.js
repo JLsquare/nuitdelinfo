140375a06755f5e2d7b6a307611415aad38a7684
raph <raphael@aime-rivard.fr>
2023-12-08 05:46:41 +0100

    avancement

--- website/src/scrollManager.js
+++ website/src/scrollManager.js
@@ -26,4 +26,29 @@ export function initScrollManager() {
         });
 
     });
+
+    const transition2Element = document.getElementById("transition2");
+    const transition2Rect = transition2Element.getBoundingClientRect();
+    const transition2Top = 6000  ;
+
+    const transition2 = {
+        fromPixel: transition2Top,
+        toPixel: transition2Top + viewportHeight,
+        startColor:  [8, 28, 21],
+        endColor: [82, 183, 136],
+    }
+
+    window.addEventListener('scroll', () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        let factor = (scrollTop - transition2.fromPixel) / (transition2.toPixel - transition2.fromPixel);
+        console.log(scrollTop, factor);
+        factor = Math.max(0, Math.min(1, factor)); // Clamp the factor between 0 and 1
+        console.log(factor);
+
+        const interpolatedColor = interpolateColor(transition2.startColor, transition2.endColor, factor);
+        const elementsToUpdates = document.querySelectorAll(".transition2-bg")
+        elementsToUpdates.forEach((element) => {
+            element.style.backgroundColor = rgbToCss(interpolatedColor);
+        });
+    });
 }
