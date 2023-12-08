68e2c2c2349fcb532fdd640f2e98a0c1b77d32b0
JL² <jeanloup.mellion@gmail.com>
2023-12-08 06:16:12 +0100

    fix tetris magnifique

--- website/src/tetris.js
+++ website/src/tetris.js
@@ -68,6 +68,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function createEnemyCanvas(id) {
+        const totalEnemyCanvases = document.querySelectorAll('[id^="enemyCanvas-"]').length;
+
+        if (totalEnemyCanvases >= 4) {
+            return;
+        }
+
         let newCanvas = document.createElement('canvas');
         newCanvas.id = `enemyCanvas-${id}`;
         newCanvas.width = 200;
