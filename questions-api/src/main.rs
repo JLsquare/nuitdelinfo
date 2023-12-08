ad915505b4d7583947f543f1ddedb510c6fd64a8
GatienDR <GatienDoesStuff@users.noreply.github.com>
2023-12-08 04:54:37 +0100

    ajout API questions

---
+++ questions-api/src/main.rs
@@ -0,0 +1,43 @@
+use actix_web::web::Data;
+use actix_web::Result;
+use actix_web::{get, web, App, HttpServer};
+use rand::{thread_rng, Rng};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct QuizzEntries {
+    questions: Vec<QuizzEntry>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct QuizzEntry {
+    question: String,
+    options: [String; 4],
+    answer: usize,
+    explanation: String,
+}
+
+#[get("/question")]
+async fn get_question(data: web::Data<QuizzEntries>) -> Result<web::Json<QuizzEntry>> {
+    let mut rng = thread_rng();
+
+    let index = rng.gen_range(0..data.questions.len());
+
+    Ok(web::Json(data.questions[index].clone()))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let questions = include_str!("../questions.json");
+    let questions = serde_json::from_str::<QuizzEntries>(questions).unwrap();
+    let questions = Data::new(questions);
+
+    HttpServer::new(move || App::new().service(get_question).app_data(questions.clone()))
+        .bind("localhost:8080")
+        .unwrap()
+        .run()
+        .await
+        .unwrap();
+
+    Ok(())
+}
